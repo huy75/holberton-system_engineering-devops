27ef860beb5b6c0269f126346f4ac8afbb122e97
huy75 <huy.nguyen66@gmail.com>
2021-02-08 16:41:21 +0000

    feat zombie processes

--- 0x05-processes_and_signals/102-zombie.c
+++ 0x05-processes_and_signals/102-zombie.c
@@ -1,1 +1,39 @@
-#!/usr/bin/env bash
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+/**
+ * infinite_while - infinite loop
+ *
+ * Return: 0
+ */
+int infinite_while(void)
+{
+	while (1)
+		sleep(1);
+
+	return (0);
+}
+
+/**
+ * main - Create 5 zombie processes
+ * Return: 0
+ */
+int main(void)
+{
+	unsigned int idx;
+	size_t pid;
+
+	for (idx = 0; idx < 5; idx++)
+	{
+		pid = fork();
+		if (pid == 0)
+		{
+			printf("Zombie process created, PID: %d\n", getpid());
+			exit(0);
+		}
+	}
+	infinite_while();
+
+	return (0);
+}
