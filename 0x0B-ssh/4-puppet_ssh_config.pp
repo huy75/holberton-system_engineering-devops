aa54b7b434f516ee3192392120999f05bd599e8b
huy75 <huy.nguyen66@gmail.com>
2021-04-04 17:03:05 +0000

    feat advance task

---
+++ 0x0B-ssh/4-puppet_ssh_config.pp
@@ -0,0 +1,15 @@
+# Turns off password authentication and uses holberton key
+
+file_line {'Turn off passwd auth':
+    ensure  => present,
+    path    => '/etc/ssh/ssh_config',
+    line    => '    PasswordAuthentication no',
+    replace => true
+}
+
+file_line {'Declare identity file':
+    ensure  => present,
+    path    => '/etc/ssh/ssh_config',
+    line    => '    IdentityFile ~/.ssh/holberton',
+    replace => true
+}
