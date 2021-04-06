cc9e6063e3e09278113305a76299f470526589fd
huy75 <huy.nguyen66@gmail.com>
2021-04-06 14:14:52 +0000

    fix task 7

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,27 +1,23 @@
 # This manifest installs ngix and adds redirect page
 
 package { 'nginx':
-  ensure => present,
+  ensure => installed,
   name   => 'nginx',
 }
 
 file { '/usr/share/nginx/html/index.html':
-  ensure  => present,
   path    => '/usr/share/nginx/html/index.html',
   content => 'Holberton School for the win!',
 }
 
 file_line { 'redirect':
-  ensure => present,
+  ensure => 'present',
   path   => '/etc/nginx/sites-available/default',
   after  => 'listen 80 default_server;',
   line   => 'rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;',
 }
 
 service { 'nginx':
-  ensure     => running,
-  enable     => true,
-  hasrestart => true,
-  require    => Package['nginx'],
-  subscribe  => File_line['redirect_me'],
+  ensure  => 'running',
+  require => Package['nginx'],
 }
