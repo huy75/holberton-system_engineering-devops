f8a40d48480850f21efc778c55a53a2f8c7fc227
huy75 <huy.nguyen66@gmail.com>
2021-04-07 08:49:25 +0000

    fix 7

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -11,13 +11,14 @@ file { '/usr/share/nginx/html/index.html':
 }
 
 file_line { 'redirect':
-  ensure => 'present',
+  ensure => present,
   path   => '/etc/nginx/sites-available/default',
   after  => 'listen 80 default_server;',
   line   => 'rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;',
 }
 
 service { 'nginx':
-  ensure  => 'running',
+  ensure  => running,
   require => Package['nginx'],
+  subscribe  => File_line['redirect'],
 }
