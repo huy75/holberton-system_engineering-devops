6743c992e61473d5847f6fc0b71704f5f7b1118c
huy75 <huy.nguyen66@gmail.com>
2021-04-12 16:01:25 +0000

    fix add require P[nginx]

--- 0x0F-load_balancer/2-puppet_custom_http_response-header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response-header.pp
@@ -16,20 +16,24 @@ file {'/var/www/html/index.html':
 }
 
 file_line { 'redirect_me':
-  ensure => present,
-  path   => '/etc/nginx/sites-available/default',
-  after  => 'listen 80 default_server;',
-  line   => 'rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;',
+  ensure  => present,
+  path    => '/etc/nginx/sites-available/default',
+  after   => 'listen 80 default_server;',
+  line    => 'rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;',
+  require => Package['nginx'],
 }
 
 file_line { 'cstHeader':
-  ensure  => 'present',
+  ensure  => present,
   path    => '/etc/nginx/sites-available/default',
   after   => 'listen 80 default_server;',
   line    => 'add_header X-Served-By $hostname;',
+  require => Package['nginx'],
 }
 
 service { 'nginx':
   ensure     => running,
+  enable     => true,
+  hasrestart => true,
   require    => Package['nginx'],
 }
