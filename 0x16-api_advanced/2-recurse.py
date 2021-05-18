60fc84ec4090a08c5904d0b0bd7d1c077471e9b0
huy75 <huy.nguyen66@gmail.com>
2021-05-18 11:20:02 +0000

    fix test status_code

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -3,19 +3,21 @@
 import requests
 
 
-def recurse(subreddit, hot_list=[], after=None):
+def recurse(subreddit, hot_list=[], after=''):
     URL = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
     headers = {"User-agent": "custom"}
     params = {'after': after}
 
     res = requests.get(URL, headers=headers, params=params,
-                       allow_redirects=False).json()
-    data = res['data']
-    if data is None:
-        return None
+                       allow_redirects=False)
+
+    if (res.status_code != 200):
+        return (None)
+
+    data = res.json()['data']
+    for each in data['children']:
+        hot_list.append(each['data']['title'])
     after = data['after']
-    for item in data['children']:
-        hot_list.append(item['data']['title'])
     if after is not None:
         recurse(subreddit, hot_list, after)
     return hot_list if len(hot_list) > 0 else None
